149761c6111b17f3a3c9cb34721965cca828a1e9
ef-code <111162892+EF-Code@users.noreply.github.com>
2026-08-13 05:57:57 +0100

    style: add TypeScript ESLint configuration

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import eslint from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['coverage/**', 'dist/**', 'node_modules/**'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    rules: {
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        {
+          checksVoidReturn: false,
+        },
+      ],
+    },
+  },
+);
